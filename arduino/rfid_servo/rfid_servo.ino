3e86b3eb1e7136d118e1f7618ba679119fb1d3ca
yanyiju <yanyiju@umich.edu>
2020-02-22 15:40:40 -0800

    rfid_servo control uploaded

---
+++ arduino/rfid_servo/rfid_servo.ino
@@ -0,0 +1,55 @@
+#include <Servo.h>
+#include <SPI.h>
+#include <MFRC522.h>
+
+#define RST_PIN         9
+#define SS_PIN          10
+
+MFRC522 mfrc522(SS_PIN, RST_PIN);
+Servo myServo;
+
+void setup() {
+  Serial.begin(9600);    // Initialize serial communications with the PC
+  while (!Serial);    // Do nothing if no serial port is opened (added for Arduinos based on ATMEGA32U4)
+  SPI.begin();      // Init SPI bus
+  mfrc522.PCD_Init();   // Init MFRC522
+  myServo.attach(3);
+  myServo.write(0);
+  Serial.println("Please put your key on the reader...");
+}
+
+void loop() {
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! mfrc522.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) {
+    return;
+  }
+
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+  
+  Serial.println();
+  Serial.print("Message : ");
+  content.toUpperCase();
+
+  // key tag: 89 7F 97 D5; card tag: B7 08 7B 44  
+  if (content.substring(1) == "89 7F 97 D5") {
+    Serial.println("Authorized access");
+    Serial.println();
+    myServo.write(180);
+    delay(5000);
+    myServo.write(0);
+  }
+}
