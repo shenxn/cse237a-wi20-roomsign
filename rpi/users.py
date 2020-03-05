2cab124f2dfb1856d35458c98662d1e76a24c575
Xiaonan Shen <s@sxn.dev>
2020-03-05 00:43:27 -0800

    Modify users.py

--- rpi/users.py
+++ rpi/users.py
@@ -1,10 +1,10 @@
 users = {
     'shenxn.io@gmail.com': {
-        'name': 'Xiaonan S.',
+        'name': 'gmail user',
         'key_id': [0x89, 0x7F, 0x97, 0xD5]
     },
     'xis079@eng.ucsd.edu': {
-        'name': 'xis079 UCSD',
+        'name': 'UCSD user',
         'key_id': [0xB7, 0x08, 0x7B, 0x44]
     }
 }
